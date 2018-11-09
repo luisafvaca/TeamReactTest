6dae353493824878c2ce3edbb40b4aa45c313493
Luisa Fernanda Vaca Correa <luisa.vaca@globant.com>
2018-11-09 09:47:23 -0500

    TEAM-initial-commit

---
+++ src/Actions/getStoreData.js
@@ -0,0 +1,30 @@
+export const STORE_REQUEST_INIT = "STORE_REQUEST_INIT";
+export const STORE_REQUEST_SUCCESS = "STORE_REQUEST_SUCCESS";
+export const STORE_REQUEST_FAILURE = "STORE_REQUEST_FAILURE";
+
+export const initRequestStore = () => ({
+    type: STORE_REQUEST_INIT
+})
+
+export const successRequestStore = (data) => ({
+    type: STORE_REQUEST_SUCCESS,
+    data
+})
+
+export const failureRequestStore = (error) => ({
+    type: STORE_REQUEST_FAILURE,
+    error
+})
+
+export function getStoreData(dispatch){
+    return (dispatch) => {
+        dispatch(initRequestStore());
+        fetch('http://localhost:8000/store').then((response) => {
+            return response.json()
+        }).then((data) => {
+            dispatch(successRequestStore(data))
+        }).catch((error) => {
+            dispatch(failureRequestStore(error))
+        })
+    }
+}
